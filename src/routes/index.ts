52070d99e2a2ab70bd436dbf0f912e5c1a5a66e0
Daniel Adegoke <danyadegoke@gmail.com>
2021-06-28 07:29:16 +0100

    file structure + example code + env and jwt handled

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 
 import { userResource } from './user';
+// import { validateSession } from './auth';
 
 const router = express.Router();
 
 router.get('/', (_, res) => res.json({ success: true, message: 'User gateway v1 up.' }));
+
+// router.use(validateSession);
 router.post('/user', userResource);
 
 export default router;
