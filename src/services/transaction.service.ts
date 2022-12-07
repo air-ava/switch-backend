465fccfeb6e21ea0a738c7adbaf1566596c49dbd
Daniel Adegoke <danyadegoke@gmail.com>
2022-12-07 13:16:36 +0100

    chore: cleanup

--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -1,5 +1,5 @@
 import { log } from 'winston';
-import { getTransactionsREPO } from '../database/repositories/transaction.repo';
+import { getOneTransactionREPO, getTransactionsREPO } from '../database/repositories/transaction.repo';
 import { sendObjectResponse, BadRequestException } from '../utils/errors';
 import { Log } from '../utils/logs';
 
@@ -17,14 +17,14 @@ export const listTransactions = async (data: any): Promise<any> => {
 };
 
 export const getTransaction = async (data: any): Promise<any> => {
-    const { user_id, id } = data;
-    try {
-      const existingTransaction = await getTransactionsREPO({ user_id, id }, []);
-      if (!existingTransaction) throw Error('Transaction not found');
-  
-      return sendObjectResponse('Transaction retrieved successfully', existingTransaction);
-    } catch (e: any) {
-      console.log({ e });
-      return BadRequestException(e.message || 'Transaction retrieval failed, kindly try again');
-    }
-  };
+  const { user_id, id } = data;
+  try {
+    const existingTransaction = await getOneTransactionREPO({ user_id, id }, []);
+    if (!existingTransaction) throw Error('Transaction not found');
+
+    return sendObjectResponse('Transaction retrieved successfully', existingTransaction);
+  } catch (e: any) {
+    console.log({ e });
+    return BadRequestException(e.message || 'Transaction retrieval failed, kindly try again');
+  }
+};
