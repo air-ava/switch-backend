3053147af193f6707418742106163cc458bc1a38
Daniel Adegoke <danyadegoke@gmail.com>
2023-03-30 06:50:50 +0100

    fix: bank Transfer

--- src/validators/payment.validator.ts
+++ src/validators/payment.validator.ts
@@ -5,3 +5,13 @@ export const updateBankTransfer = joi.object().keys({
   reference: joi.string().required(),
   status: joi.string().valid('PROCESSING', 'PROCESSED').required(),
 });
+
+export const completeBankTransfer = joi.object().keys({
+  reference: joi.string().required(),
+  user: joi.string().required(),
+  bankDraftCode: joi.string().required(),
+  documents: joi.array().items(joi.string()).required(),
+  bankName: joi.string().required(),
+  accountName: joi.string().required(),
+  accountNumber: joi.string().required(),
+});
