109ac23a729b8a2ad657ae428e9b1f4f3ec1626c
Daniel Adegoke <danyadegoke@gmail.com>
2023-07-25 10:22:18 +0100

    Fix: Table re-organize

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -109,14 +109,18 @@ export const createObjectFromArray = (payload: any, key: string, value: any, pat
   return response;
 };
 
-export const createObjectFromArrayWithoutValue = (arr: any, key: string) => {
+export const createObjectFromArrayWithoutValue = (arr: any, key1: string, key2: string) => {
   return arr.reduce((result: any, item: any) => {
-    const keyValue = item[key];
-    if (!result[keyValue]) {
+    // const keyValue1 = item[key1];
+    const response = jsonify(item);
+    const keyValue1 = key1.includes('.') ? findObjectValue(item, key1) : response[key1];
+    const keyValue2 = key2.includes('.') ? findObjectValue(item, key2) : response[key2];
+    const combinedKey = `${keyValue1} | ${keyValue2}`;
+    if (!result[combinedKey]) {
       // eslint-disable-next-line no-param-reassign
-      result[keyValue] = [];
+      result[combinedKey] = [];
     }
-    result[keyValue].push(item);
+    result[combinedKey].push(item);
     return result;
   }, {});
 }
