882550bc32cbf41d938e16ccf0dd91f5714baa24
Daniel Adegoke <danyadegoke@gmail.com>
2023-08-03 10:17:35 +0100

    fix: commented out code

--- src/database/migrations/1691053772041-AddDefaultEmailColumnToStudents.ts
+++ src/database/migrations/1691053772041-AddDefaultEmailColumnToStudents.ts
@@ -2,17 +2,17 @@ import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 
 export class AddDefaultEmailColumnToStudents1691053772041 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    // await queryRunner.addColumn(
-    //   'students',
-    //   new TableColumn({
-    //     name: 'defaultEmail',
-    //     type: 'boolean',
-    //     default: true,
-    //   }),
-    // );
+    await queryRunner.addColumn(
+      'students',
+      new TableColumn({
+        name: 'defaultEmail',
+        type: 'boolean',
+        default: true,
+      }),
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    // await queryRunner.dropColumn('students', 'defaultEmail');
+    await queryRunner.dropColumn('students', 'defaultEmail');
   }
 }
