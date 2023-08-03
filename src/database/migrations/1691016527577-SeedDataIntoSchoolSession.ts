a418c9b1cd15ad35506bb38bb68eabb4f41d8798
Daniel Adegoke <danyadegoke@gmail.com>
2023-08-03 11:06:22 +0100

    fix: Update the seeding of School Sessions

--- src/database/migrations/1691016527577-SeedDataIntoSchoolSession.ts
+++ src/database/migrations/1691016527577-SeedDataIntoSchoolSession.ts
@@ -1,10 +1,10 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 
 const seedData = [
   {
     codePrefix: 'shs_',
     name: 'school_session',
-    column: `'session', 'country', 'schedule_id', 'name', 'status', 'start_date', 'expiry_date', 'education_level'`,
+    column: '`session`, `country`, `schedule_id`, `name`, `status`, `start_date`, `expiry_date`, `education_level`',
     rows: [
       `'2022/2023', 'UGANDA', 11, '2022/2023', 1, null, '2023-12-01', 'Primary'`,
       `'2022/2023', 'UGANDA', 12, '2022/2023', 10, null, '2023-12-01', 'Nursery'`,
@@ -15,12 +15,29 @@ const seedData = [
   },
 ];
 
+const columnDetails = {
+  name: 'code',
+  type: 'varchar',
+};
+
 export class SeedDataIntoSchoolSession1691016527577 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.changeColumns(seedData[0].name, [
+      {
+        oldColumn: new TableColumn({ ...columnDetails, isNullable: false }),
+        newColumn: new TableColumn({ ...columnDetails, isNullable: true }),
+      },
+    ]);
     await Promise.all(
       seedData.map((seed) => seed.rows.map((value) => queryRunner.query(`INSERT INTO ${seed.name} (${seed.column}) VALUES (${value})`))),
     );
     await queryRunner.query(`UPDATE ${seedData[0].name} SET code = CONCAT('${seedData[0].codePrefix}', SUBSTRING(REPLACE(UUID(), '-', ''), 1, 17))`);
+    await queryRunner.changeColumns(seedData[0].name, [
+      {
+        oldColumn: new TableColumn({ ...columnDetails, isNullable: true }),
+        newColumn: new TableColumn({ ...columnDetails, isNullable: false }),
+      },
+    ]);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
