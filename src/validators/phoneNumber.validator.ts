227f64fefd8ad71a90f450bf31aa55c193c86c29
Daniel Adegoke <danyadegoke@gmail.com>
2023-09-11 03:41:25 +0100

    feat: extend USSD payment

--- src/validators/phoneNumber.validator.ts
+++ src/validators/phoneNumber.validator.ts
@@ -6,6 +6,14 @@ export const phoneNumberValidator = joi.object({
   localFormat: joi.string().required(),
 });
 
+export const phoneValidator = joi.object({
+  phone: joi
+    .string()
+    .regex(/^256\d{9}$/)
+    .message('Please enter a valid Ugandan phone number with the country code (256)')
+    .required(),
+});
+
 export const ImageValidator = joi.object({
   url: joi.string().uri().required(),
   table_type: joi.string().valid('image', 'business', 'cart', 'order', 'product', 'transactions').required(),
