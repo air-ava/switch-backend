3eb8b799e0af1f5637fe3c1861c036f2f397f1b4
Daniel Adegoke <danyadegoke@gmail.com>
2023-12-13 04:36:42 +0100

    chore: Clean up Code

--- src/protofuncts/notification.method.ts
+++ src/protofuncts/notification.method.ts
@@ -1,11 +1,5 @@
 import BankTransferService from '../services/bankTransfer.service';
-
 import { ControllerResponse } from '../dto/responseInterfaces.dto';
-// import { wrapGrpcUnaryCall } from '../utils/response';
 import ResponseService from '../utils/response';
 
 export const notifySlack = ResponseService.wrapGrpcUnaryCall<{ type: string }, ControllerResponse>(BankTransferService.notifySlack);
-
-// export const retrieveBillCategories = ResponseService.wrapGrpcUnaryCall<{ billerCode: string }, { categories: IBillCategory[] }, ControllerResponse>(
-//   getBillersCategories,
-// );
