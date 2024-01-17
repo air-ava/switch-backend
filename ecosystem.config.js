a181a847bb2b94c974d36cfd6a12bd3caaf3bdfe
Daniel Adegoke <danyadegoke@gmail.com>
2024-01-17 03:21:02 +0100

    feat: update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   apps: [
-    // {
-    //   name: 'base-service',
-    //   script: 'npm',
-    //   args: 'run start',
-    // },
+    {
+      name: 'base-service',
+      script: 'npm',
+      args: 'run start',
+    },
     {
       script: 'build/consumer/reviewCustomer.consumer.js',
       name: 'review-customer-consumer',
